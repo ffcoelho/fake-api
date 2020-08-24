bfc1942e8d648635fefa8529029b523ecd3dba10
ffcoelho <ffcoelho.dev@gmail.com>
2020-08-23 23:33:19 -0300

    swagger

--- api/open/apiRequestOpen.ts
+++ api/open/apiRequestOpen.ts
@@ -3,5 +3,5 @@ import { UserRequestHandler } from "../../model/express.model";
 const User = require("../../data/dbUser");
 
 export const apiRequestOpen: UserRequestHandler = async (req, res, next) => {
-  res.status(200).json({ data: req.role });
+  res.status(200).json({ role: req.role });
 };
--- api/routerApi.ts
+++ api/routerApi.ts
@@ -1,20 +1,20 @@
 import { Router } from "express";
+import swaggerUi from 'swagger-ui-express';
 import { apiAuthRouter } from "./auth/apiAuthRouter";
 import { apiOpenRouter } from "./open/apiOpenRouter";
 import { apiSecureRouter } from "./secure/apiSecureRouter";
 import { userAuth } from "../middleware/userAuth";
 import { userRole } from "../middleware/userRole";
+import { swaggerDoc } from './swagger.json';
 
 import { apiMockRouter } from "./mock/apiMockRouter";
 
 export const routerApi = Router();
 
-routerApi.get("/", (req, res) => {
-  res.send("fake-api-helper");
-});
-
 routerApi.use("/auth", apiAuthRouter);
 routerApi.use("/open", userRole, apiOpenRouter);
 routerApi.use("/secure", userAuth, apiSecureRouter);
+routerApi.use("/", swaggerUi.serve);
+routerApi.get("/", swaggerUi.setup(swaggerDoc));
 
 routerApi.use("/mock", apiMockRouter);
