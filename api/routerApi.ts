f3936ba8279b17e1ac0c7e9601794723ec0b5b49
ffcoelho <ffcoelho.dev@gmail.com>
2020-08-24 21:37:25 -0300

    swagger opts

--- api/routerApi.ts
+++ api/routerApi.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import swaggerUi from 'swagger-ui-express';
+import swaggerUi, { SwaggerUiOptions } from 'swagger-ui-express';
 import { apiAuthRouter } from "./auth/apiAuthRouter";
 import { apiOpenRouter } from "./open/apiOpenRouter";
 import { apiSecureRouter } from "./secure/apiSecureRouter";
@@ -7,10 +7,18 @@ import { userAuth } from "../middleware/userAuth";
 import { userRole } from "../middleware/userRole";
 import { swaggerDoc } from './swagger.json';
 
+const swaggerOpts: SwaggerUiOptions = {
+  customSiteTitle: "Fake API Helper",
+  swaggerOptions: {
+    supportedSubmitMethods: [],
+    docExpansion: "none"
+  }
+};
+
 export const routerApi = Router();
 
 routerApi.use("/auth", apiAuthRouter);
 routerApi.use("/open", userRole, apiOpenRouter);
 routerApi.use("/secure", userAuth, apiSecureRouter);
 routerApi.use("/", swaggerUi.serve);
-routerApi.get("/", swaggerUi.setup(swaggerDoc));
+routerApi.get("/", swaggerUi.setup(swaggerDoc, swaggerOpts));
--- build/api/routerApi.js
+++ build/api/routerApi.js
@@ -12,9 +12,16 @@ const apiSecureRouter_1 = require("./secure/apiSecureRouter");
 const userAuth_1 = require("../middleware/userAuth");
 const userRole_1 = require("../middleware/userRole");
 const swagger_json_1 = require("./swagger.json");
+const swaggerOpts = {
+    customSiteTitle: "Fake API Helper",
+    swaggerOptions: {
+        supportedSubmitMethods: [],
+        docExpansion: "none"
+    }
+};
 exports.routerApi = express_1.Router();
 exports.routerApi.use("/auth", apiAuthRouter_1.apiAuthRouter);
 exports.routerApi.use("/open", userRole_1.userRole, apiOpenRouter_1.apiOpenRouter);
 exports.routerApi.use("/secure", userAuth_1.userAuth, apiSecureRouter_1.apiSecureRouter);
 exports.routerApi.use("/", swagger_ui_express_1.default.serve);
-exports.routerApi.get("/", swagger_ui_express_1.default.setup(swagger_json_1.swaggerDoc));
+exports.routerApi.get("/", swagger_ui_express_1.default.setup(swagger_json_1.swaggerDoc, swaggerOpts));
