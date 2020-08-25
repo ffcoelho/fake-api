6d23e09208b1935bbcd5f3a23f5c6997b389e653
ffcoelho <ffcoelho.dev@gmail.com>
2020-08-24 22:07:04 -0300

    auth route

--- api/auth/apiAuthRouter.ts
+++ api/auth/apiAuthRouter.ts
@@ -1,9 +1,7 @@
 import { Router } from "express";
 import { jsonParser } from "../../middleware/bodyParser";
-import { apiPostAuthSignIn } from "./apiPostAuthSignIn";
 import { apiPostAuthLogin } from "./apiPostAuthLogin";
 
 export const apiAuthRouter = Router();
 
-apiAuthRouter.route("/signin").post(jsonParser, apiPostAuthSignIn);
-apiAuthRouter.route("/login").post(jsonParser, apiPostAuthLogin);
+apiAuthRouter.route("/").post(jsonParser, apiPostAuthLogin);
--- build/api/auth/apiAuthRouter.js
+++ build/api/auth/apiAuthRouter.js
@@ -2,8 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const bodyParser_1 = require("../../middleware/bodyParser");
-const apiPostAuthSignIn_1 = require("./apiPostAuthSignIn");
 const apiPostAuthLogin_1 = require("./apiPostAuthLogin");
 exports.apiAuthRouter = express_1.Router();
-exports.apiAuthRouter.route("/signin").post(bodyParser_1.jsonParser, apiPostAuthSignIn_1.apiPostAuthSignIn);
-exports.apiAuthRouter.route("/login").post(bodyParser_1.jsonParser, apiPostAuthLogin_1.apiPostAuthLogin);
+exports.apiAuthRouter.route("/").post(bodyParser_1.jsonParser, apiPostAuthLogin_1.apiPostAuthLogin);
