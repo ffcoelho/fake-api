150f042ddfa0924b011f2e1e647253a8f9b21455
ffcoelho <ffcoelho.dev@gmail.com>
2020-08-27 20:16:45 -0300

    bld

--- build/api/auth/apiAuthRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const bodyParser_1 = require("../../middleware/bodyParser");
-const apiPostAuthLogin_1 = require("./apiPostAuthLogin");
-exports.apiAuthRouter = express_1.Router();
-exports.apiAuthRouter.route("/").post(bodyParser_1.jsonParser, apiPostAuthLogin_1.apiPostAuthLogin);
--- build/api/secure/apiRequestSecure.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const User = require("../../data/dbUser");
-exports.apiRequestSecure = async (req, res, next) => {
-    res.status(200).json({ role: req.role });
-};
