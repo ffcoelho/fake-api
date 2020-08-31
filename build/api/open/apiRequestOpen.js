391379477fe5934bed0850e43d18702bb7c1e96a
ffcoelho <ffcoelho.dev@gmail.com>
2020-08-30 23:31:11 -0300

    bld

--- build/api/open/apiRequestOpen.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const User = require("../../data/dbUser");
-exports.apiRequestOpen = async (req, res, next) => {
-    if (req.fakeApiError) {
-        return res.status(req.fakeApiError.status).json({ role: req.role, error: req.fakeApiError.message });
-    }
-    res.status(200).json({ role: req.role });
-};
