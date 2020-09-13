49454cc3c738543e99745397eb5e07feaa019700
ffcoelho <ffcoelho.dev@gmail.com>
2020-09-13 03:36:57 -0300

    remove swagger-ui

--- api/routerApi.ts
+++ api/routerApi.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import swaggerUi, { SwaggerUiOptions } from 'swagger-ui-express';
 import { jsonParser } from "../middleware/bodyParser";
 import { fakeStatusHandler } from "../middleware/fakeStatusHandler";
 import { userRole } from "../middleware/userRole";
@@ -7,15 +6,6 @@ import { apiGetDocs } from "./apiGetDocs";
 import { apiAuthRouter } from "./auth/apiAuthRouter";
 import { apiFileRouter } from "./file/apiFileRouter";
 import { apiMockRouter } from "./mock/apiMockRouter";
-import { swaggerDoc } from './swagger.json';
-
-const swaggerOpts: SwaggerUiOptions = {
-  customSiteTitle: "Fake API Helper",
-  swaggerOptions: {
-    supportedSubmitMethods: [],
-    docExpansion: "none"
-  }
-};
 
 export const routerApi = Router();
 
@@ -23,5 +13,3 @@ routerApi.use("/auth", jsonParser, fakeStatusHandler, apiAuthRouter);
 routerApi.use("/file", jsonParser, userRole, fakeStatusHandler, apiFileRouter);
 routerApi.use("/mock", jsonParser, userRole, fakeStatusHandler, apiMockRouter);
 routerApi.use("/", apiGetDocs);
-routerApi.use("/swagger", swaggerUi.serve);
-routerApi.get("/swagger", swaggerUi.setup(swaggerDoc, swaggerOpts));
