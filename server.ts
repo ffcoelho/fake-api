9e65a90d8d92d0b3bd1a2d939ad87660cb0df1cb
ffcoelho <ffcoelho.dev@gmail.com>
2020-09-17 21:40:37 -0300

    enable cors

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { connectDB } from "./data/db";
 import { routerApi } from "./api/routerApi";
 
@@ -7,6 +8,7 @@ app.disable("x-powered-by");
 
 connectDB();
 
+app.use(cors());
 app.use(express.static('public'));
 app.use("/", routerApi);
 app.listen(process.env.PORT || 9000, () => console.log(`fake-api-helper started and listening on port: ${process.env.PORT || 9000}`));
